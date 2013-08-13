b8e5603da3c29300cd3638168fb6a1acaf55c338
Drew Maxwell <Andrew.Stuart.Maxwell@gmail.com>
2013-08-13 12:41:52 -0500

    Vector Math
    Expanded vector functions.

--- DragonScale/Vector.cpp
+++ DragonScale/Vector.cpp
@@ -1,16 +1,116 @@
 #include "Vector.h"
 
+#include <cassert>
+
 using namespace Math;
 
-Vector4::Vector4( void ) {
+Vector3::Vector3( void ) {
+	x = y = z;
 }
 
-Vector4::Vector4( float x = 0.0f, float y = 0.0f, float z = 0.0f, float w = 0.0f ) {
-	this->x = x;
-	this->y = y;
-	this->z = z;
-	this->w = w;
+Vector3::Vector3( float xx = 0.0f, float yy = 0.0f, float zz = 0.0f )
+	: x( xx ), y( yy ), z( zz ) {
 }
 
-Vector4::~Vector4( void ) {
+Vector3::~Vector3( void ) {
+}
+
+float Vector3::LengthSquared( void ) const {
+	float x2 = x * x;
+	float y2 = y * y;
+	float z2 = z * z;
+
+	return x2 + y2 + z2;
+}
+
+float Vector3::Length( void ) const {
+	return sqrtf( LengthSquared() );
+}
+
+Vector3 Vector3::operator+( const Vector3& v ) const {
+	float tx = x + v.x;
+	float ty = y + v.y;
+	float tz = z + v.z;
+
+	return Vector3( tx, ty, tz );
+}
+
+Vector3 Vector3::operator-( const Vector3& v ) const {
+	float tx = x - v.x;
+	float ty = y - v.y;
+	float tz = z - v.z;
+
+	return Vector3( tx, ty, tz );
+}
+
+Vector3 Vector3::operator*( const float s ) const {
+	float tx = s * x;
+	float ty = s * y;
+	float tz = s * z;
+
+	return Vector3( tx, ty, tz );
+}
+
+Vector3 Vector3::operator/( const float s ) const {
+	assert( s != 0 );
+	
+	float inv = 1.0f / s;
+
+	float tx = inv * x;
+	float ty = inv * y;
+	float tz = inv * z;
+
+	return Vector3( tx, ty, tz );
+}
+
+Vector3& Vector3::operator+=( const Vector3& v ) {
+	x += v.x;
+	y += v.y;
+	z += v.z;
+
+	return *this;
+}
+
+Vector3& Vector3::operator-=( const Vector3& v ) {
+	x -= v.x;
+	y -= v.y;
+	z -= v.z;
+
+	return *this;
+}
+
+Vector3& Vector3::operator*=( const float s ) {
+	x *= s;
+	y *= s;
+	z *= s;
+
+	return *this;
+}
+
+Vector3& Vector3::operator/=( const float s ) {
+	assert( s != 0 );
+
+	float inv = 1.0f / s;
+
+	x *= inv;
+	y *= inv;
+	z *= inv;
+
+	return *this;
+}
+
+Vector3 Vector3::operator-( void ) const {
+	return Vector3( -x, -y, -z );
+}
+
+float Vector3::operator[]( int i ) const {
+	assert( i >= 0 && i <= 2 );
+
+	return (&x)[i];
+}
+
+float& Vector3::operator[]( int i ) {
+	assert( i >= 0 && i <= 2 );
+
+	return (&x)[i];
 }
--- DragonScale/Vector.h
+++ DragonScale/Vector.h
@@ -1,19 +1,68 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
+#include <cmath>
+
 namespace Math {
 
-class Vector4 {
+class Vector3 {
 public:
-	Vector4( void );
-	~Vector4( void );
-	Vector4( float x, float y, float z, float w );
+	Vector3( void );
+	~Vector3( void );
+	Vector3( float x, float y, float z );
+
+	float LengthSquared( void ) const;
+	float Length( void ) const;
+
+	Vector3 operator+( const Vector3& v ) const;
+	Vector3 operator-( const Vector3& v ) const;
+	Vector3 operator*( const float s ) const;
+	Vector3 operator/( const float s ) const;
+
+	Vector3& operator+=( const Vector3& v );
+	Vector3& operator-=( const Vector3& v );
+	Vector3& operator*=( const float s );
+	Vector3& operator/=( const float s );
+
+	Vector3 operator-( void ) const;
+
+	float operator[]( int i ) const;
+	float& operator[]( int i );
 
 	float x;
 	float y;
 	float z;
-	float w;
 };
+
+inline float Dot( const Vector3& v1, const Vector3& v2 ) {
+	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+inline float AbsDot( const Vector3& v1, const Vector3& v2 ) {
+	return fabsf( Dot( v1, v2 ) );
+}
+
+inline Vector3 Cross( const Vector3& v1, const Vector3& v2 ) {
+	return Vector3( ( v1.y * v2.z ) - ( v1.z * v2.y ),
+					( v1.z * v2.x ) - ( v1.x * v2.z ),
+					( v1.x * v2.y ) - ( v1.y * v2.x ) );
+}
+
+inline Vector3 Normalize( const Vector3& v ) {
+	return ( v / v.Length() );
+}
+
+inline void CoordinateSystem( const Vector3& v1, Vector3* v2, Vector3* v3 ) {
+	if ( fabsf( v1.x ) > fabsf( v1.y ) ) {
+		float invLen = 1.0f / sqrtf( v1.x * v1.x + v1.z * v1.z );
+		*v2 = Vector3( -v1.z * invLen, 0.0f, v1.x * invLen );
+	} else {
+		float invLen = 1.0f / sqrtf( v1.y * v1.y + v1.z * v1.z );
+		*v2 = Vector3( 0.0f, v1.z * invLen, -v1.y * invLen );
+	}
+
+	*v3 = Cross( v1, *v2 );
+}
 
 }
 
